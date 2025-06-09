425a5015088dd3e6cbf4161781f276a5382c57b5
ATUL ANAND <128018790+Atul2512anand@users.noreply.github.com>
2025-06-09 08:20:49 +0530

    Create ReportDetail.js

---
+++ client/src/pages/ReportDetail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ReportDetail = () => {
+  return (
+    <div>
+      <h2>Report Detail Page (Placeholder)</h2>
+      <p>This page is under construction.</p>
+    </div>
+  );
+};
+
+export default ReportDetail;
